4365e34c6da8a61a67386ca275d4aaecef6ed6ef
22CEAS <43822218+22CEAS@users.noreply.github.com>
2021-01-22 10:03:03 -0500

    Se modifico DBManager

--- SGA_PCR/AccesoDatos/DBManager.cs
+++ SGA_PCR/AccesoDatos/DBManager.cs
@@ -30,10 +30,10 @@ namespace AccesoDatos
         //public static string cadena = "server=" + "localhost" + "; user=root; password=root; database=" + "bd_leasein" + "; SslMode=None;Connect Timeout=300000";
 
         //local steven
-        static string cadena = "server=" + "localhost" + "; user=root; password=ROOT; port=3305; database=" + "test_leasein" + "; SslMode=None;Connect Timeout=300000";
+        //static string cadena = "server=" + "localhost" + "; user=root; password=ROOT; port=3305; database=" + "test_leasein" + "; SslMode=None;Connect Timeout=300000";
 
         //PRUEBAS SERVIDOR 16gb
-        //public static string cadena = "server=" + "154.16.171.82" + "; user=root; password=leasein1; port=16258; database=" + "leasein_16gb" + "; SslMode=None;Connect Timeout=300000";
+        public static string cadena = "server=" + "204.2.195.84" + "; user=root; password=rootroot; port=18886; database=" + "test" + "; SslMode=None;Connect Timeout=300000";
 
         //public static string cadena = "server=" + "209.45.53.41" + "; user=root; password=root; database=" + "bd_leasein" + "; SslMode=None;Connect Timeout=300000";
         public MySqlConnection conexion = null;
